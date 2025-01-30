1af5709843617efa7d7e64914c66783c0aed6f01
Franck <52790721+franck-armand@users.noreply.github.com>
2025-01-30 16:58:22 +0100

    - added room dependencies to build.gradle.kts and app/build.gradle.kts - added dependencies: splashscreen, constraintlayout, jtds, and datalogic-android-sdk - updated versions for: constraintlayout, and appcompat - added schemaDirectory for room database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("androidx.room")
 }
 
 android {
@@ -15,7 +16,6 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -29,20 +29,33 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    room {
+        schemaDirectory("$projectDir/schemas")
+    }
 }
 
 dependencies {
+    // To use room
+    val room_version = "2.6.1"
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
 
+    // Ui dependencies
     implementation(libs.appcompat)
     implementation(libs.material)
+    implementation("androidx.core:core-splashscreen:1.0.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0")
+    implementation("androidx.appcompat:appcompat:1.7.0-alpha04")
+
+    // external lib and dependencies
     implementation(files("libs/BT_A700.jar"))
+    implementation("net.sourceforge.jtds:jtds:1.3.1")
     implementation("com.github.datalogic:datalogic-android-sdk:1.42")
+
+    // Test dependencies
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("androidx.core:core-splashscreen:1.0.0")
-    implementation("net.sourceforge.jtds:jtds:1.3.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
-    // To use constraintlayout in compose
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0")
+
 }
