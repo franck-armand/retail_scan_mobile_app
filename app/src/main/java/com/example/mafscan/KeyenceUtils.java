4c8528d25302e499e2fabd050cfc3a6ee1bd0305
Franck <52790721+franck-armand@users.noreply.github.com>
2025-02-10 12:01:20 +0100

    - commented out the Keyence scanner code, to remove dependency.

--- app/src/main/java/com/example/mafscan/KeyenceUtils.java
+++ app/src/main/java/com/example/mafscan/KeyenceUtils.java
@@ -1,66 +1,66 @@
-package com.example.mafscan;
-
-import android.content.Context;
-import android.util.Log;
-
-import com.keyence.autoid.sdk.scan.DecodeResult;
-import com.keyence.autoid.sdk.scan.ScanManager;
-
-public class KeyenceUtils implements ScanManager.DataListener {
-    private static ScanManager mScanManager;
-    private static ScanListener mScanListener;
-    private static final String TAG = "KeyenceUtils";
-    public interface ScanListener {
-        void onScan(String scannedData, String codeType);
-    }
-
-    public static boolean initializeScanner(Context context) {
-        if (mScanManager == null) {
-            mScanManager = ScanManager.createScanManager(context);
-            if (mScanManager != null) {
-                mScanManager.addDataListener(new KeyenceUtils());
-                Log.d(TAG, "Scanner initialized successfully");
-                return true;
-            } else {
-                Log.e(TAG, "Failed to initialize scanner");
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static void setScanListener(ScanListener listener) {
-        mScanListener = listener;
-    }
-
-    public static void stopScanning() {
-        if (mScanManager != null) {
-            Log.d(TAG, "Stopping scanning");
-            mScanManager.stopRead();
-        } else{
-            Log.e(TAG, "Scanner not initialized");
-        }
-    }
-
-    public static void releaseScanner() {
-        if (mScanManager != null) {
-            Log.d(TAG, "Releasing scanner");
-            mScanManager.releaseScanManager();
-            mScanManager = null;
-        } else{
-            Log.e(TAG, "Scanner not initialized");
-        }
-    }
-
-    @Override
-    public void onDataReceived(DecodeResult decodeResult) {
-        String scannedData = decodeResult.getData();
-        String codeType = decodeResult.getCodeType();
-        Log.d(TAG, "onDataReceived: " + scannedData + ", Code Type: " + codeType);
-        if (mScanManager.isReading()) {
-            if (mScanListener != null) {
-                mScanListener.onScan(scannedData, codeType);
-            }
-        }
-    }
-}
+//package com.example.mafscan;
+//
+//import android.content.Context;
+//import android.util.Log;
+//
+//import com.keyence.autoid.sdk.scan.DecodeResult;
+//import com.keyence.autoid.sdk.scan.ScanManager;
+//
+//public class KeyenceUtils implements ScanManager.DataListener {
+//    private static ScanManager mScanManager;
+//    private static ScanListener mScanListener;
+//    private static final String TAG = "KeyenceUtils";
+//    public interface ScanListener {
+//        void onScan(String scannedData, String codeType);
+//    }
+//
+//    public static boolean initializeScanner(Context context) {
+//        if (mScanManager == null) {
+//            mScanManager = ScanManager.createScanManager(context);
+//            if (mScanManager != null) {
+//                mScanManager.addDataListener(new KeyenceUtils());
+//                Log.d(TAG, "Scanner initialized successfully");
+//                return true;
+//            } else {
+//                Log.e(TAG, "Failed to initialize scanner");
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//
+//    public static void setScanListener(ScanListener listener) {
+//        mScanListener = listener;
+//    }
+//
+//    public static void stopScanning() {
+//        if (mScanManager != null) {
+//            Log.d(TAG, "Stopping scanning");
+//            mScanManager.stopRead();
+//        } else{
+//            Log.e(TAG, "Scanner not initialized");
+//        }
+//    }
+//
+//    public static void releaseScanner() {
+//        if (mScanManager != null) {
+//            Log.d(TAG, "Releasing scanner");
+//            mScanManager.releaseScanManager();
+//            mScanManager = null;
+//        } else{
+//            Log.e(TAG, "Scanner not initialized");
+//        }
+//    }
+//
+//    @Override
+//    public void onDataReceived(DecodeResult decodeResult) {
+//        String scannedData = decodeResult.getData();
+//        String codeType = decodeResult.getCodeType();
+//        Log.d(TAG, "onDataReceived: " + scannedData + ", Code Type: " + codeType);
+//        if (mScanManager.isReading()) {
+//            if (mScanListener != null) {
+//                mScanListener.onScan(scannedData, codeType);
+//            }
+//        }
+//    }
+//}
