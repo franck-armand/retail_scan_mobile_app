21596a8c49095a5e20c177bb0d700b4461a59331
Franck <52790721+franck-armand@users.noreply.github.com>
2025-02-14 16:07:14 +0100

    - Implemented scanner trigger support for scan authentication. - Added a scan listener to handle scanned data and update the UI. - Added two tabs: one for LDAP authentication and the other for scan authentication. - Disabled scanner triggers during LDAP authentication. - Enabled scanner triggers during scan authentication. - Updated the UI to show/hide layouts according to the authentication method. - Cleared, set and disabled the field after scan authentication tab is selected. - Added methods to parse scanned data and update the state of the scan authentication button.

--- app/src/main/java/com/example/mafscan/LoginActivity.java
+++ app/src/main/java/com/example/mafscan/LoginActivity.java
@@ -3,9 +3,9 @@ package com.example.mafscan;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,41 +16,65 @@ import com.google.android.material.textfield.TextInputEditText;
 import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity implements
-        TabLayout.OnTabSelectedListener {
+        TabLayout.OnTabSelectedListener, DataLogicUtils.ScanListener {
 
-    TextInputEditText usernameEditText;
-    TextInputEditText passwordEditText;
-    Button validateLdapButton;
+    TextInputEditText ldapUsernameEditText;
+    TextInputEditText ldapPasswordEditText;
+    TextInputEditText scanUsernameEditText;
+    Button authenticationLdapButton;
+    Button authenticationScanButton;
+    LinearLayout ldapLayout;
+    LinearLayout scanLayout;
+    private static final String TAG = LoginActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        usernameEditText = findViewById(R.id.usernameEditText);
-        passwordEditText = findViewById(R.id.passwordEditText);
-        validateLdapButton = findViewById(R.id.validateLdapButton);
+        // Initialize the scanner
+        DataLogicUtils.initializeScanner(this);
+
+        // Set the scan listener
+        DataLogicUtils.setScanListener(this);
+
+        ldapUsernameEditText = findViewById(R.id.ldapUsernameEditText);
+        ldapPasswordEditText = findViewById(R.id.ldapPasswordEditText);
+        authenticationLdapButton = findViewById(R.id.validateLdapButton);
+        authenticationScanButton = findViewById(R.id.validateScanButton);
+        scanUsernameEditText = findViewById(R.id.scanUsernameEditText);
+        ldapUsernameEditText.addTextChangedListener(LoginTextWatcher);
+        ldapPasswordEditText.addTextChangedListener(LoginTextWatcher);
 
         TabLayout tabLayout = findViewById(R.id.tabLayout);
         tabLayout.addOnTabSelectedListener(this);
 
-        usernameEditText.addTextChangedListener(LoginTextWatcher);
-        passwordEditText.addTextChangedListener(LoginTextWatcher);
+        ldapLayout = findViewById(R.id.ldapLayout);
+        scanLayout = findViewById(R.id.scanLayout);
+
     }
 
     @Override
     public void onTabSelected(TabLayout.Tab tab) {
-        LinearLayout ldapLayout = findViewById(R.id.ldapLayout);
-        LinearLayout scanLayout = findViewById(R.id.scanLayout);
         if (tab.getPosition() == 0) { // LDAP Auth
             ldapLayout.setVisibility(View.VISIBLE);
             scanLayout.setVisibility(View.GONE);
+            // Disable triggers for LDAP Auth
+            DataLogicUtils.setTriggersEnabled(false);
         } else if (tab.getPosition() == 1) { // Scan Auth
-            EditText scanUsernameResult = findViewById(R.id.scanUsernameResult);
-            scanUsernameResult.setEnabled(false);
-            ldapLayout.setVisibility(View.GONE);
-            scanLayout.setVisibility(View.VISIBLE);
+            // Enable triggers for Scan Auth
+            DataLogicUtils.setTriggersEnabled(true);
+            // Clear field
+            clearSetAndDisableField();
+            updateScanButtonState();
         }
+    }
+
+    private void clearSetAndDisableField() {
+        scanUsernameEditText.setText("");
+        scanUsernameEditText.setEnabled(false);
+        ldapLayout.setVisibility(View.GONE);
+        scanLayout.setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -77,9 +101,30 @@ public class LoginActivity extends AppCompatActivity implements
     };
 
     private void updateLoginButtonState() {
-        String username = Objects.requireNonNull(usernameEditText.getText()).toString().trim();
-        String password = Objects.requireNonNull(passwordEditText.getText()).toString().trim();
+        String username = Objects.requireNonNull(ldapUsernameEditText.getText()).toString().trim();
+        String password = Objects.requireNonNull(ldapPasswordEditText.getText()).toString().trim();
+        authenticationLdapButton.setEnabled(!username.isEmpty() && !password.isEmpty());
+    }
 
-        validateLdapButton.setEnabled(!username.isEmpty() && !password.isEmpty());
+    @Override
+    public void onScan(String scannedData, String codeType) {
+        Log.d(TAG, "Scanned Data: " + scannedData + ", Code Type: " + codeType);
+        // Parse and Update the UI with the scan result
+        parseScannedData(scannedData);
+        // Update the scan button state
+        updateScanButtonState();
+
+    }
+
+    private void parseScannedData(String scannedData) {
+        // TODO : Place holder to format the scan result accordingly later
+        // Split the data into key-value pairs
+        String[] pairs = scannedData.trim().split("\\|");
+        scanUsernameEditText.setText(scannedData.trim());
+    }
+
+    private void updateScanButtonState() {
+        String scanResult = scanUsernameEditText.getText().toString().trim();
+        authenticationScanButton.setEnabled(!scanResult.isEmpty());
     }
 }
