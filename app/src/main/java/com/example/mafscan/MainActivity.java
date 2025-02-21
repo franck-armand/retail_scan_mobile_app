83a520f94dde69a2e42ffd8bc7f7e024e4cfbca7
Franck <52790721+franck-armand@users.noreply.github.com>
2025-02-21 15:43:26 +0100

    - updated the table layout weights for key and value text views in RetrieveScanInfo - stopped scanning and disabled triggers when the activity is paused in RetrieveScanInfo

--- app/src/main/java/com/example/mafscan/MainActivity.java
+++ app/src/main/java/com/example/mafscan/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.mafscan;
 
 
+import static android.webkit.ConsoleMessage.MessageLevel.LOG;
+
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.VibrationEffect;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
 
@@ -39,9 +44,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home3);
 
+        // Get app version
+        String appVersion = getAppVersion();
+
         // Set up the custom Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            String title = getString(R.string.app_name) + " _ " + appVersion;
+            getSupportActionBar().setTitle(title);
+        }
+
 
         drawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
@@ -89,6 +102,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         getOnBackPressedDispatcher().addCallback(this, onBackPressedCallback);
     }
 
+    private String getAppVersion() {
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            return packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.d(TAG, "Error getting app version", e);
+            return "Unknown";
+        }
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -110,7 +133,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                     new fragment_about()).commit();
         } else if (item.getItemId() == R.id.nav_login) {
-//            Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
 //            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
 //                    new fragment_login()).commit();
             Intent intent = new Intent(this, LoginActivity.class);
@@ -121,89 +143,3 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
 }
-
-//public class MainActivity extends AppCompatActivity {
-//    private DrawerLayout drawerLayout;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//
-//        // SplashScreen
-//        SplashScreen.installSplashScreen(this);
-//
-//        // initialize Vibrator
-//        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-//        final VibrationEffect clickVibration = VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK);
-//
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_home3);
-//
-//        // Set up the custom Toolbar
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        drawerLayout = findViewById(R.id.drawer_layout);
-//        NavigationView navigationView = findViewById(R.id.nav_view);
-//        //navigationView.setNavigationItemSelectedListener(this);
-//
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
-//                R.string.open_nav,
-//                R.string.close_nav);
-//        drawerLayout.addDrawerListener(toggle);
-//        toggle.syncState();
-//        if (savedInstanceState == null) {
-//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-//                    new fragment_home()).commit();
-//            navigationView.setCheckedItem(R.id.nav_home);
-//        }
-//
-//        // Set up Scan button navigation
-//        LinearLayout scanButton = findViewById(R.id.btn_scan);
-//        scanButton.setOnClickListener(v -> {
-//            vibrator.vibrate(clickVibration);
-//            Intent intent = new Intent(MainActivity.this, ScanActivityFromTo.class);
-//            startActivity(intent);
-//        });
-//
-//        // set up Retrieve QR code info button navigation
-//        LinearLayout retrieveButton = findViewById(R.id.retrieve_scan_info);
-//        retrieveButton.setOnClickListener(v -> {
-//            vibrator.vibrate(clickVibration);
-//            Intent intent = new Intent(MainActivity.this, RetrieveScanInfo.class);
-//            startActivity(intent);
-//        });
-//    }
-//
-//    @Override
-//    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-//        switch (item.getItemId()) {
-//            case R.id.nav_home:
-//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-//                        new fragment_home()).commit();
-//                break;
-//            case R.id.nav_settings:
-//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-//                        new fragment_settings()).commit();
-//                break;
-//            case R.id.nav_about:
-//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-//                        new fragment_about()).commit();
-//                break;
-//            case R.id.nav_logout:
-//                Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
-//                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-//                        new fragment_login()).commit();
-//                break;
-//        }
-//        drawerLayout.closeDrawer(GravityCompat.START);
-//        return true;
-//    }
-//    @Override
-//    public void onBackPressed() {
-//        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-//            drawerLayout.closeDrawer(GravityCompat.START);
-//        } else {
-//            super.onBackPressed();
-//        }
-//    }
-//}
