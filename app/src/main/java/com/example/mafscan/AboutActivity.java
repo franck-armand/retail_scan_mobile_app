554d2170a8099249fc67c64205fbe52e3eabceb0
Franck <52790721+franck-armand@users.noreply.github.com>
2025-02-24 11:10:56 +0100

    - Implemented an about activity to display app information, key features, release notes, help/support, and legal details. - Added the AboutActivity to the AndroidManifest. - Added fragment_about layout for the AboutActivity content.

---
+++ app/src/main/java/com/example/mafscan/AboutActivity.java
@@ -0,0 +1,40 @@
+package com.example.mafscan;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
+
+public class AboutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_about);
+
+        // Set up toolbar
+        Toolbar toolbar = findViewById(R.id.aboutToolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        // Get the app version
+        String appVersion = Utils.getAppVersion(this);
+        TextView appVersionTextView = findViewById(R.id.app_version);
+        appVersionTextView.setText(appVersion);
+
+        // Get and set app release date and version
+        TextView releaseNoteTitle = findViewById(R.id.release_notes_title);
+        String title = getString(R.string.release_notes) + " " + appVersion;
+        releaseNoteTitle.setText(title);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavUtils.navigateUpFromSameTask(this);
+        return true;
+    }
+}
