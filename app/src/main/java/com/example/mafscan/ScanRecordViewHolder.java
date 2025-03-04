9786a3efa7b802b974904ca73a884dafbf24058c
Franck <52790721+franck-armand@users.noreply.github.com>
2025-03-04 16:53:09 +0100

    - Implemented ScanRecordAdapter and ScanRecordViewHolder for displaying scan records in a RecyclerView. - Added the logic to display the scan data, type, date, count, and sent status in the ScanRecordViewHolder. - Implemented an item click listener in ScanRecordAdapter to handle item clicks. - Updated the date format in the view holder to be more user-friendly.

---
+++ app/src/main/java/com/example/mafscan/ScanRecordViewHolder.java
@@ -0,0 +1,52 @@
+package com.example.mafscan;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class ScanRecordViewHolder extends RecyclerView.ViewHolder {
+
+    TextView scannedDataTextView;
+    TextView codeTypeTextView;
+    TextView scanDateTextView;
+    TextView scanCountTextView;
+    TextView scanStatusTextView;
+    private final String TAG = getClass().getSimpleName();
+
+    public ScanRecordViewHolder(@NonNull View itemView) {
+        super(itemView);
+        scannedDataTextView = itemView.findViewById(R.id.scannedDataTextView);
+        codeTypeTextView = itemView.findViewById(R.id.codeTypeTextView);
+        scanDateTextView = itemView.findViewById(R.id.scanDateTextView);
+        scanCountTextView = itemView.findViewById(R.id.scanCountTextView);
+        scanStatusTextView = itemView.findViewById(R.id.scanStatusTextView);
+    }
+
+    public void bind(ScanRecord scanRecord) {
+        scannedDataTextView.setText(scanRecord.scannedData);
+        codeTypeTextView.setText(scanRecord.codeType);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss",
+                Locale.getDefault());
+        try {
+            Date date = dateFormat.parse(scanRecord.scanDate);
+            SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm a",
+                    Locale.getDefault());
+            scanDateTextView.setText(outputFormat.format(date));
+        } catch (Exception e) {
+            Log.e(TAG, "Error parsing date: " + e.getMessage());
+        }
+        scanCountTextView.setText(String.valueOf(scanRecord.scanCount));
+        if (scanRecord.isSentToServer == 1) {
+            scanStatusTextView.setText("Sent");
+        } else {
+            scanStatusTextView.setText("Not Sent");
+        }
+    }
+}
