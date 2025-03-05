08d2096d47a92ea2e73ea6d3581251be8bf487a5
Franck <52790721+franck-armand@users.noreply.github.com>
2025-03-05 08:13:44 +0100

    - sorted the list of scan sessions by session creation date in descending order (newest first) in the view model

--- app/src/main/java/com/example/mafscan/FailedOrSavedScanViewModel.java
+++ app/src/main/java/com/example/mafscan/FailedOrSavedScanViewModel.java
@@ -5,7 +5,9 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Transformations;
 
+import java.util.Collections;
 import java.util.List;
 
 public class FailedOrSavedScanViewModel extends AndroidViewModel {
@@ -16,7 +18,11 @@ public class FailedOrSavedScanViewModel extends AndroidViewModel {
     public FailedOrSavedScanViewModel(@NonNull Application application) {
         super(application);
         repository = new FailedOrSavedScanRepository(application);
-        allScanSessions = repository.getAllScanSessions();
+        allScanSessions = Transformations.map(repository.getAllScanSessions(), scanSessions -> {
+            // Sort the list by sessionCreationDate in descending order (newest first)
+            scanSessions.sort((s1, s2) -> s2.sessionCreationDate.compareTo(s1.sessionCreationDate));
+            return scanSessions;
+        });
     }
 
     public LiveData<List<ScanSession>> getAllScanSessions() {
