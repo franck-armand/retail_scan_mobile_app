e11379c3cee828b8a62a423bcfe17e56158ed7fd
Franck <52790721+franck-armand@users.noreply.github.com>
2025-03-11 12:25:53 +0100

    - removed unused import statements in FailedOrSavedScanDao and ScanRecordDao.

--- app/src/main/java/com/example/mafscan/FailedOrSavedScanDao.java
+++ app/src/main/java/com/example/mafscan/FailedOrSavedScanDao.java
@@ -2,7 +2,6 @@ package com.example.mafscan;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
-import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Transaction;
 
--- app/src/main/java/com/example/mafscan/ScanRecordDao.java
+++ app/src/main/java/com/example/mafscan/ScanRecordDao.java
@@ -3,7 +3,6 @@ package com.example.mafscan;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
-import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
 
