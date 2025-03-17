bc3b01efb5eef352d0134399b989b8e3eee709aa
Franck <52790721+franck-armand@users.noreply.github.com>
2025-03-17 12:40:59 +0100

    - added a new menu for the failed/saved scan activity - added a new session option to the menu that will start a new scan session. - added an option to view scan info - added an about option - added an authentication option - added a settings option - added a help option - added icons to menu options - added a menu to the failed/saved activity.

--- app/src/main/java/com/example/mafscan/FailedOrSavedScanActivity.java
+++ app/src/main/java/com/example/mafscan/FailedOrSavedScanActivity.java
@@ -1,15 +1,22 @@
 package com.example.mafscan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.LinkedList;
 import java.util.Objects;
 
 public class FailedOrSavedScanActivity extends AppCompatActivity {
@@ -57,4 +64,39 @@ public class FailedOrSavedScanActivity extends AppCompatActivity {
             failedScansRecyclerView.setVisibility(View.VISIBLE);
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Utils.inflateMenu(this, menu, R.menu.failed_saved_menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if (id == R.id.failed_saved_action_Help) {
+            Utils.showHelpDialog(getSupportFragmentManager());
+            return true;
+        } else if (id == R.id.failed_saved_action_about) {
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.failed_saved_action_auth) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.failed_saved_action_settings) {
+            Utils.showToast(this, "Settings not implemented yet!", 0);
+            return true;
+        } else if (id == R.id.failed_saved_new_session) {
+            Intent intent = new Intent(this, ScanFromToActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.failed_saved_scan_info){
+            Intent intent = new Intent(this, RetrieveScanInfoActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
+
