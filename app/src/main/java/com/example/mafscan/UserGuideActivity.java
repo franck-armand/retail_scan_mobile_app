ff0277792099ef3f1cb5c1040567add78a2d5518
Franck <52790721+franck-armand@users.noreply.github.com>
2025-03-17 13:25:09 +0100

    - Added a menu resource for the user guide activity. - Implemented menu options in UserGuideActivity: New Session, Scan Info, About, Authentication, Settings, and Help. - Updated UserGuideActivity to handle the user guide menu.

--- app/src/main/java/com/example/mafscan/UserGuideActivity.java
+++ app/src/main/java/com/example/mafscan/UserGuideActivity.java
@@ -1,6 +1,9 @@
 package com.example.mafscan;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -39,5 +42,38 @@ public class UserGuideActivity extends AppCompatActivity {
                             break;
                     }
                 }).attach();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Utils.inflateMenu(this, menu, R.menu.user_guide_menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if (id == R.id.user_guide_action_Help) {
+            Utils.showHelpDialog(getSupportFragmentManager());
+            return true;
+        } else if (id == R.id.user_guide_action_about) {
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.user_guide_action_auth) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.user_guide_action_settings) {
+            Utils.showToast(this, "Settings not implemented yet!", 0);
+            return true;
+        } else if (id == R.id.user_guide_new_session) {
+            Intent intent = new Intent(this, ScanFromToActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.user_guide_scan_info){
+            Intent intent = new Intent(this, RetrieveScanInfoActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
