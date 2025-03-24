e4c7d67a52baf157456eab525bef669cab9cab38
Franck <52790721+franck-armand@users.noreply.github.com>
2025-03-24 12:36:17 +0100

    - Added a constant for date format. - Updated the name of the EMPTY SHUTTLE scan session.

--- app/src/main/java/com/example/mafscan/Constants.java
+++ app/src/main/java/com/example/mafscan/Constants.java
@@ -3,10 +3,11 @@ package com.example.mafscan;
 public class Constants {
     public static final int TAB_RECEPTION = 0;
     public static final int TAB_EXPEDITION = 1;
+    public static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
     public static final String LOCATION_CODE_PREFIX = "(00)LOC";
     public static final String SCAN_SESSION_RECEPTION = "RECEPTION";
     public static final String SCAN_SESSION_TRANSFER = "TRANSFER";
     public static final String SCAN_SESSION_EXPEDITION = "EXPEDITION";
     public static final String SCAN_SESSION_INVENTORY = "INVENTORY";
-    public static final String SCAN_SESSION_SHUTTLE = "EMPTY SHUTTLE";
+    public static final String SCAN_SESSION_SHUTTLE = "EMPTY_SHUTTLE";
 }
